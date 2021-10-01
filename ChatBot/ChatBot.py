82f0a52bc278588e2f8c88a468583d087f5f60e4
Halahan_Kirill <Galagan.k.a.01@gmail.com>
2021-10-01 11:02:43 +0300

    ChatBot 2-nd stage

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -1,4 +1,7 @@
-def predstavlenie():
-    print("Hello! My name is GalaganK519st_bot")
-    print("I was created in 3021")
+print("Hello! My name is GalaganK519st_bot")
+print("I was created in 3021")
+
+name = input("Please, remind your name\n >")
+print("Have a nice name,", name)
+
 
