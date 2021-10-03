732dd56694289d385af4d95f637c979e35f7bc21
Halahan_Kirill <Galagan.k.a.01@gmail.com>
2021-10-03 12:24:17 +0300

    ChatBot 4-s stage

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -11,3 +11,11 @@ rem5 = int(input("remainder5:>"))
 rem7 = int(input("remainder7:>"))
 age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
 print("your age is,", age, ",its best time to start programing!")
+
+print("Now I will prove you that I can count to any number you want")
+x = 0
+y = int(input(">"))
+while x <= y:
+    print(x, "!")
+    x = x + 1
+print("Completed, have a nice day!")
