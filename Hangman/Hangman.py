3dd288bf5ee1a868ede964a890f65e7eef04052b
Halahan_Kirill <Galagan.k.a.01@gmail.com>
2021-10-20 09:46:37 +0300

    Hangman stage 2

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,6 +1,17 @@
 def announce():
     print("HANGAMAN")
-    print("The game will avalible soon")
+    # print("The game will avalible soon")
 
 
 announce()
+
+
+def main():
+    word = input("Guess the word:>")
+    if word == "Python":
+        print("You guess the word,", word, "\n You survived")
+    else:
+        print("You lose, try again")
+
+
+main()
