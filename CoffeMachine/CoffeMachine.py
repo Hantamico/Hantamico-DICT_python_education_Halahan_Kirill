71366f0e84f7b13402839c756740e2a9138f57ed
Halahan_Kirill <Galagan.k.a.01@gmail.com>
2021-10-21 08:35:41 +0300

    CoffeMachine stage 1

---
+++ CoffeMachine/CoffeMachine.py
@@ -0,0 +1,9 @@
+
+
+print("Starting to make a coffee")
+print("Grinding coffee beans")
+print("Boiling water")
+print("Mixing boiled water with crushed coffee beans")
+print("Pouring coffee in to the cup")
+print("Pouring some milk in to the cup")
+print("Coffee is ready")
