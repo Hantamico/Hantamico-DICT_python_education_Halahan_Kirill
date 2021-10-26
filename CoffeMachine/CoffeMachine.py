f77739ea7d02cd68e948e3c08d0dac8b535af7f0
Halahan_Kirill <Galagan.k.a.01@gmail.com>
2021-10-26 22:26:29 +0300

    Coffe Machine stage 5

--- CoffeMachine/CoffeMachine.py
+++ CoffeMachine/CoffeMachine.py
@@ -30,6 +30,10 @@ milk = 540
 cups = 9
 
 
+class ResourceError(Exception):
+    pass
+
+
 def print_parametrs():
     print("Coffee machine has:")
     print(money, "UAH")
@@ -40,39 +44,63 @@ def print_parametrs():
 
 
 def select_action():
-    return input("CHOOSE ACTION\n-buy\n-fill\n-take\n>")
+    return input("CHOOSE ACTION\n-buy\n-fill\n-take\n-remaining\n-exit\n>")
 
 
-def select_drink():
-    return int(input("What are you want to buy\n1-espresso\n2-latte\n3-cappuccino\n>"))
+def select_drink() -> int:
+    back = input("What are you want to buy\n1-espresso\n2-latte\n3-cappuccino\nback - to main menu\n>")
+    if back == "back":
+        return main()
+    return int(back)
+
+
+def is_enough(need_water=0, need_milk=0, need_coffee=0):
+    if water < need_water:
+        print("Sorry, not enough water!\n")
+        raise ResourceError
+    if milk < need_milk:
+        print("Sorry, not enough milk!\n")
+        raise ResourceError
+    if coffee < need_coffee:
+        print("Sorry, not enough beans!\n")
+        raise ResourceError
+    if cups < 1:
+        print("Sorry, not enough cups\n")
+        raise ResourceError
+    print("I have enough resources, making you a coffee!\n")
 
 
 def buy_drinks():
     global money, coffee, milk, water, cups
     drink = select_drink()
 
-    if drink == 1:
-        assert coffee >= 250
-        assert water >= 16
-        money += 4
-        coffee -= 16
-        water -= 250
-    elif drink == 2:
-        assert coffee >= 20
-        assert water >= 350
-        assert milk >= 75
-        money += 7
-        water -= 350
-        milk -= 75
-    elif drink == 3:
-        assert coffee >= 12
-        assert water >= 200
-        assert milk >= 100
-        money += 6
-        water -= 200
-        milk -= 100
+    try:
+        if drink == 0:
+            pass
+        if drink == 1:
+            is_enough(need_water=250, need_coffee=16)
+            money += 4
+            water -= 250
+            coffee -= 16
+            cups -= 1
+        elif drink == 2:
+            is_enough(need_water=350, need_coffee=20, need_milk=75)
 
-    cups -= 1
+            money += 7
+            water -= 350
+            milk -= 75
+            coffee -= 20
+            cups -= 1
+        elif drink == 3:
+            is_enough(need_water=350, need_milk=100, need_coffee=12)
+            money += 6
+            water -= 200
+            milk -= 100
+            cups -= 1
+        else:
+            raise ValueError
+    except ResourceError:
+        pass
 
 
 def fill_machine():
@@ -92,16 +120,19 @@ def take_money():
 def main():
     print_parametrs()
 
-    action = select_action()
-    if action == "buy":
-        buy_drinks()
-    elif action == "fill":
-        fill_machine()
-    elif action == "take":
-        take_money()
+    while True:
+        action = select_action()
 
-    print()
-    print(print_parametrs())
+        if action == "buy":
+            buy_drinks()
+        elif action == "fill":
+            fill_machine()
+        elif action == "take":
+            take_money()
+        elif action == "exit":
+            exit()
+        elif action == "remaining":
+            print_parametrs()
 
 
 main()
